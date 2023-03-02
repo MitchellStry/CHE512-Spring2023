bf780032e1975c084306b6c8af113eccf3360509
Mohammad Shakiba <mshakiba@srv-p22-12.cbls.ccr.buffalo.edu>
2023-03-02 10:10:54 -0500

    Added sin(x) power series

--- MohammadShakiba/HW2.py
+++ MohammadShakiba/HW2.py
@@ -10,7 +10,8 @@ def sqroots(a,b,c):
     return x1, x2
 
 print(sqroots(10, 3, -1.5))
-def taylor_series(x, n):
+
+def taylor_series1(x, n):
     """
     This function comptues the 1/(1-x) using Taylor series expansion
     """
@@ -23,5 +24,24 @@ def taylor_series(x, n):
  
     return taylor, exact
 
-print(taylor_series(10, 100))
-#def 
+print(taylor_series1(0.5, 100))
+
+def taylor_series2(x, n):
+    """
+    This function comptues the 1/(1-x) using Taylor series expansion
+    """
+    taylor = 0.0
+    exact = cmath.sin(x)
+    c = 1
+    x2 = x**2
+    for i in range(1,n):
+        taylor += c*x
+        c /= ((2*i+1)*(2*i))
+        x *= x2
+        c *= -1
+ 
+    return taylor, exact
+
+print(taylor_series2(cmath.pi, 100))
+
+
