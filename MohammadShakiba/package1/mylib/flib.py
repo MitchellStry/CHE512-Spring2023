665b9248755eddffd3e9511358e14feab6cafdff
Mohammad Shakiba <mshakiba@srv-p22-12.cbls.ccr.buffalo.edu>
2023-03-14 10:08:05 -0400

    added files from last class

---
+++ MohammadShakiba/package1/mylib/flib.py
@@ -0,0 +1,40 @@
+import math
+
+def f1(x):
+    """
+    Linear function
+
+    Args:
+        x (float): The value
+
+    Returns:
+        x (float): Linear function of x
+    """
+    return x
+
+def f2(x):
+    """
+    Square function
+
+    Args:
+        x (float): The value
+
+    Returns:
+        x**2 (float): Square of x
+    """
+    return x**2
+
+def f3(x):
+    """
+    Sine function
+
+    Args:
+        x (float): The value
+
+    Returns:
+        x (float): Sine function of x
+    """
+    return (math.sin(x))**2
+
+
+
